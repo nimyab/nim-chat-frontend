42eb10cea8de2056d3579b71315c73cda2878490
nimyab <artyomzholobov2004@gmail.com>
2024-11-14 21:34:04 +0300

    fix menu

--- app/(tabs)/menu/index.tsx
+++ app/(tabs)/menu/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from 'react-native';
 
 export default function Menu() {
   return (
-    <View>
-      <Text>Menu</Text>
+    <View className="flex flex-col justify-center items-center h-full px-2 py-3 bg-gray-900">
+      <Text className="text-2xl text-white">Menu</Text>
     </View>
   );
 }
